23cb95e3f1d3fdb2fd5611d1893abb93207705e4
Joshua Nwankwo <joshua.nwankwo@mebank.com.au>
2020-07-17 22:15:06 +1000

    Refactoring

--- const_example/main.cpp
+++ const_example/main.cpp
@@ -1,7 +1,6 @@
 
 #include <iostream>
 #include <utility>
-
 #include "logger.h"
 using std::cout;
 using std::endl;
@@ -19,9 +18,10 @@ class Person {
     this->lastName = std::move(lastName);
     this->income = income;
     this->age = age;
+    LOG("Constructing... " << this)
   }
 
-  ~Person(){LOG("Destructing " << this)}
+  ~Person(){LOG("Destructing... " << this)}
 
   std::string GetFullName() {
     return firstName + " " + lastName;
@@ -41,33 +41,45 @@ class Person {
   void SetFirstName(std::string firstName) { this->firstName = firstName; }
 };
 
+
+void logDetails(const Person& josh, Person* pJosh, Person& rJosh);
+void logDetails2(Person josh, Person* pJosh, Person& rJosh);
 int main() {
-  Person josh("Joshua", "Uzo", 789.47, 90);
+  Person  josh("Joshua", "Uzo", 789.47, 90);
+  Person jClone = josh; //jClone is a copy of josh
+  Person* pJosh = &josh; //pJosh is a Person pointer
+  Person& rJosh = josh; //rJosh is an alias that refers to josh
+  logDetails(josh, pJosh, rJosh);
+  logDetails2(josh, pJosh, rJosh);
 
-  Person* pJosh = &josh;
-  Person& rJosh = josh;
-
-  LOG(pJosh->GetFullName())
-  LOG(rJosh.GetFullName())
-  LOG(josh.GetInfo())
-  cout << endl;
-
-  josh.SetFirstName("Janiyah");
-  LOG(pJosh->GetFullName())
-  LOG(rJosh.GetFullName())
-  LOG(josh.GetInfo())
-  cout << endl;
+  josh.SetFirstName("Sam");
+  logDetails(josh, pJosh, rJosh);
 
   pJosh->SetFirstName("Junia");
+  logDetails(josh, pJosh, rJosh);
+
+  rJosh.SetFirstName("Luke");
+  logDetails(josh, pJosh, rJosh);
+
+  return 0;
+}
+
+void logDetails(const Person& josh, Person* pJosh, Person& rJosh) {
   LOG(pJosh->GetFullName())
   LOG(rJosh.GetFullName())
   LOG(josh.GetInfo())
   cout << endl;
+}
 
-  rJosh.SetFirstName("Luke");
+//Unlike Java. In C++ there's a difference between pass by reference and pass by value.
+//Parameter "josh" is a copy or a value of Person josh and not a reference to Person josh.
+//This means that updating or modifying parameter "josh" does not alter the Person josh.
+//Consider making parameter "josh" a const reference and not a copy of Person josh
+void logDetails2(Person josh, Person* pJosh, Person& rJosh) {
+  printf("%p\n",&josh);
+
   LOG(pJosh->GetFullName())
   LOG(rJosh.GetFullName())
   LOG(josh.GetInfo())
-
-  return 0;
+  cout << endl;
 }
